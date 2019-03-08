819e2895b975e958c94ac463af91460f75ed8892
Josemar <josemarsilva-inmetrics@bitbucket.org>
2019-03-08 18:46:07 -0300

    .

--- src/sql/05_dml_insert_partitioned_table.sql
+++ src/sql/05_dml_insert_partitioned_table.sql
@@ -2,3 +2,9 @@ INSERT INTO tab_venda_part
 (id, empresa_id, cliente_id, dt_emissao, dt_periodo, vlr_venda )
 SELECT id, empresa_id, cliente_id, dt_emissao, dt_periodo, vlr_venda 
 FROM   tab_venda ;
+
+
+INSERT INTO tab_venda_part_cli
+(id, empresa_id, cliente_id, dt_emissao, dt_periodo, vlr_venda )
+SELECT id, empresa_id, cliente_id, dt_emissao, dt_periodo, vlr_venda 
+FROM   tab_venda ;
